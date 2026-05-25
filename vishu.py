006385da8e332e4c2bbad8f79b5a8c4ab451ebe6
vishal yadav <vishalyadav7783@gmail.com>
2026-05-25 22:49:53 +0530

    ss commit

--- vishu.py
+++ vishu.py
@@ -1,2 +1,34 @@
 print("hello world")
 print("welcome back ")
+
+
+# Simple Calculator in Python
+
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print("Result =", num1 + num2)
+
+elif choice == '2':
+    print("Result =", num1 - num2)
+
+elif choice == '3':
+    print("Result =", num1 * num2)
+
+elif choice == '4':
+    if num2 != 0:
+        print("Result =", num1 / num2)
+    else:
+        print("Division by zero is not possible")
+
+else:
+    print("Invalid choice")
