d1774e3c06c7d8960e7167eb997be9e161d794b8
vishal yadav <vishalyadav7783@gmail.com>
2026-05-29 13:17:30 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,56 @@
+# name = "shradha"
+# age=23
+# age2=age
+# price=25.99
+# print(name)
+# print(age)
+# ''''''
+# print("age2 :",age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# print("--- sum of two numbers -- ")
+# a=2
+# b=5
+# c=45
+# d=50
+# e=75
+# sum=a+b+c+d+e
+# print(sum)
+
+
+# #arithmatic operator
+# print("arithmatic operator")
+# a=20
+# b=10
+# sum=a+b
+# sub=a-b
+# mul=a*b
+# div=a/b
+# mod=a%b
+# pow=a**b
+
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+# print(mod)
+# print(pow)
+# #relational operators
+# print("---relational operators---")
+
+#input in python
+
+# enter the string types values
+# name=input("enter the your name :")
+# print("name :",name)
+
+# #int values 
+# a= int(input("enter the first values :"))
+# b=int(input("enter the second the values :"))
+# print("sum",a+b) 
+# #float values 
+# a= float(input("enter the first values :"))
+# b=float(input("enter the second the values :"))
+# print("sum",a+b) 
