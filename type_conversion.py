5ed7530b8db71b1d7701785589478cbcee759ac8
vishal yadav <vishalyadav7783@gmail.com>
2026-05-29 17:13:30 +0530

    first commit

---
+++ type_conversion.py
@@ -0,0 +1,19 @@
+# String to Integer Conversion
+
+a = "10"
+
+print("Original Value:", a)
+print("Original Data Type:", type(a)) #<str>
+
+# Convert string to integer
+b = int(a)
+
+print("\nAfter Conversion")
+print("Value of b:", b)
+print("Data Type of b:", type(b)) #int
+
+# Addition
+result = b + 5
+
+print("\nAddition Result")
+print("b + 5 =", result) #int
